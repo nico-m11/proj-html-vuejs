d820c1f88b0a878f52bdc23110abbb8dd974fd7a
Nicola <nicolamelito.11@gmail.com>
2020-12-09 16:33:50 +0100

    Creato Header

---
+++ js/script.js
@@ -0,0 +1,25 @@
+const app = new Vue ({
+  el: '#root',
+  data: {
+    link: [
+      {
+      home: 'Home',
+    },
+    {
+      pagine: 'Pages',
+    },
+    {
+      corso: 'Courses',
+    },
+    {
+      fet: 'Features',
+    },
+    {
+      blog: 'Blog'
+    },
+    {
+      shop: 'Shop',
+    },
+  ]
+  }
+})
