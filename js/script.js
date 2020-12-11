228afb81709e15b292279218e1afd594aebc2869
Nicola <nicolamelito.11@gmail.com>
2020-12-11 16:39:52 +0100

    End a Project
    Aggiunto footer e corretto bug di font, background e hover

--- js/script.js
+++ js/script.js
@@ -20,6 +20,48 @@ const app = new Vue ({
     {
       shop: 'Shop',
     },
-  ]
+  ],
+  address: [
+    {
+      indirizzo: '382 NE 191st # 87394 Miami, FL 33179-3899',
+    },
+    {
+      telefono: '+1(305) 547-9909 (9am - 5pm EST, Monday - Friday)',
+    },
+    {
+      email: 'support@maxcoach.com'
+    },
+  ],
+  explore: [
+    {
+      start: ' Start here',
+    },
+    {
+      success: ' Succes Story',
+    },
+    {
+      blog: 'Blog',
+    },
+    {
+      corse: 'Courses',
+    },
+    {
+      contatt: 'Contact us',
+    },
+  ],
+  information: [
+    {
+      menber: 'Menbership',
+    },
+    {
+      guide: 'Purchase guide',
+    },
+    {
+      privacy: 'Privacy policy',
+    },
+    {
+      terms: 'Terms of service',
+    },
+  ],
   }
 })
